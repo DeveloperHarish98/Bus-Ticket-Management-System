d1a404d64d4434d3145d8682acffcc1ea2290a90
DeveloperHarish98 <harishdubey98r@gmail.com>
2025-03-20 15:55:04 +0530

    "Added Fix"

--- src/main/java/org/springboot/BusTicketManagementSystemApplication.java
+++ src/main/java/org/springboot/BusTicketManagementSystemApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BusTicketManagementSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(BusTicketManagementSystemApplication.class, args);
+        System.out.println("Bus Ticket Management System");
     }
 }
